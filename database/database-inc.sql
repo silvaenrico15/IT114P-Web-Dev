d277f3c5a49114eb2944a4742a77c0640bd09c3e
silvaenrico <silvaenrico5@gmail.com>
2020-02-27 20:54:39 +0800

    Re-commit Database

--- database/database-inc.sql
+++ database/database-inc.sql
@@ -11,6 +11,24 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for it114p-inc
+DROP DATABASE IF EXISTS `it114p-inc`;
+CREATE DATABASE IF NOT EXISTS `it114p-inc` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `it114p-inc`;
+
+-- Dumping structure for table it114p-inc.registers
+DROP TABLE IF EXISTS `registers`;
+CREATE TABLE IF NOT EXISTS `registers` (
+  `id` int(11) NOT NULL,
+  `college` varchar(50) DEFAULT '',
+  `fullname` varchar(50) DEFAULT '',
+  `program` varchar(50) DEFAULT '',
+  `sport` varchar(50) DEFAULT '',
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
